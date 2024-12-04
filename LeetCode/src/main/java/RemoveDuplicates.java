caa7aa06a57fdb2a90d2d13872c34a6fa52592e0
Mohamed shosha <150439621+Mohamad-shosha@users.noreply.github.com>
2024-12-04 18:26:02 +0200

    Add files via upload

---
+++ LeetCode/src/main/java/RemoveDuplicates.java
@@ -0,0 +1,20 @@
+package main.java;
+
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+        int [] nums = new int [] {0,1,1,1,2,5,6,4,54,6,4,5,5,5,5,6,7};
+        System.out.println(removeDuplicates(nums));
+    }
+        public static int removeDuplicates(int[] nums) {
+            if (nums.length == 0) return 0;
+
+            int k = 1;
+            for (int i = 1; i < nums.length; i++) {
+                if (nums[i] != nums[i - 1]) {
+                    k++;
+                }
+            }
+            return k;
+        }
+    }
+
